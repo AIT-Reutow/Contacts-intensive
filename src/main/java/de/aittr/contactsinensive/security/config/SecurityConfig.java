f1603b6cd824d65f1059bb02f8ff5c037e98f811
AR1988 <37342319+AR1988@users.noreply.github.com>
2024-12-05 17:47:52 +0100

    Enhance login and logout handlers with JSON responses
    Replaced default login success and failure URLs with JSON responses to improve client-side interaction. Added JSON message handling for both successful and failed login attempts, as well as logout actions, providing clearer feedback through HTTP response. This improves user feedback mechanism by standardizing responses and utilizing appropriate HTTP status codes.

--- src/main/java/de/aittr/contactsinensive/security/config/SecurityConfig.java
+++ src/main/java/de/aittr/contactsinensive/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package de.aittr.contactsinensive.security.config;
 
 import de.aittr.contactsinensive.security.CustomAccessDeniedHandler;
 import de.aittr.contactsinensive.security.CustomAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,12 +26,21 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form
                         .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/contacts", true)
-                        .failureUrl("/login?error=true")
+                        .successHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_OK);
+                            response.getWriter().write("{\"message\": \"Login successful\"}");
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.getWriter().write("{\"error\": \"Invalid username or password\"}");
+                        })
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_OK);
+                            response.getWriter().write("{\"message\": \"Logout successful\"}");
+                        })
                 )
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
